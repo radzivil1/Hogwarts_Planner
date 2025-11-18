3572334162ca4add9cd5e97d7f5a4801dbe0e63b
radzivil1 <83864545+radzivil1@users.noreply.github.com>
2025-11-18 07:44:37 +0300

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"691bf5714cbc690025c2c7d0",
+				"691bf5714cbc690025c2c7d3"
+			]
+		},
+		"691bf5714cbc690025c2c7d0": {
+			"id": "691bf5714cbc690025c2c7d0",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"691bf5714cbc690025c2c7d3": {
+			"id": "691bf5714cbc690025c2c7d3",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
